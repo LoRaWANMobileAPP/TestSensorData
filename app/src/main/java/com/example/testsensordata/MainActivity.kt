f27cf189181166e1e71e26877b5daa5168ffe944
Jan-Robin <janrbrus@gmail.com>
2023-09-27 20:48:28 +0200

    Front End work
    Added color, and theme for background and text.
    Added text view for outdoor temp.
    Formated text string temp.
    constraints for the measurements are made

--- app/src/main/java/com/example/testsensordata/MainActivity.kt
+++ app/src/main/java/com/example/testsensordata/MainActivity.kt
@@ -6,14 +6,17 @@ import android.widget.TextView
 import kotlin.random.Random
 import android.os.Handler
 
+private const val initialValue = "20"
 class MainActivity : AppCompatActivity() {
     private lateinit var tvsensor: TextView
+    private lateinit var tvTempOut: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tvsensor =findViewById(R.id.tvSensorData)
+        tvTempOut = findViewById(R.id.tvTempOut)
 
         //Create a handler to update the textview every second.
         val handler = Handler()
@@ -24,7 +27,9 @@ class MainActivity : AppCompatActivity() {
         val UpdateTextViewRunnable = object: Runnable{
             override fun run() {
                 val sensorValue = sensorData()
-                tvsensor.text = "Sensor Data: $sensorValue"
+                val outTemp = sensorData()
+                tvsensor.text = "%.2f".format(sensorValue)+" °C"
+                tvTempOut.text = "%.2f".format(outTemp)+" °C"
                 handler.postDelayed(this, delayMillis)
             }
         }
